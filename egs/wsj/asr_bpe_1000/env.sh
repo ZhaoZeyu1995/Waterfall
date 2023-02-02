0665ff0013f54fa906c0790f11ac7a09ac6baa57
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2023-02-02 23:28:47 +0000

    change env.sh in wsj/asr_bpe_1000 back to k2-1.11 due to compatibility issue.

--- egs/wsj/asr_bpe_1000/env.sh
+++ egs/wsj/asr_bpe_1000/env.sh
@@ -1,1 +1,1 @@
-. $CONDA_PREFIX/etc/profile.d/conda.sh && conda deactivate && conda activate $LOCAL_HOME/conda/k2-1.13
+. $CONDA_PREFIX/etc/profile.d/conda.sh && conda deactivate && conda activate $LOCAL_HOME/conda/k2
