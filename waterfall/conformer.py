8ab10b5608659fd4115e60e2d4b09384c7581c2f
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2023-02-10 23:36:48 +0000

    change to use the pre-defined conv2dsubsampling2 in espnet

--- waterfall/conformer.py
+++ waterfall/conformer.py
@@ -33,70 +33,70 @@ from espnet.nets.pytorch_backend.transformer.positionwise_feed_forward import (
     PositionwiseFeedForward,  # noqa: H301
 )
 from espnet.nets.pytorch_backend.transformer.repeat import repeat
-from espnet.nets.pytorch_backend.transformer.subsampling import Conv2dSubsampling
+from espnet.nets.pytorch_backend.transformer.subsampling import Conv2dSubsampling, Conv2dSubsampling2
 
 from espnet.nets.pytorch_backend.nets_utils import make_non_pad_mask
 
 
-class Conv2dSubsampling2(nn.Module):
-    """Convolutional 2D subsampling (to 1/2 length).
+# class Conv2dSubsampling2(nn.Module):
+    # """Convolutional 2D subsampling (to 1/2 length).
 
-    Args:
-        idim (int): Input dimension.
-        odim (int): Output dimension.
-        dropout_rate (float): Dropout rate.
-        pos_enc (torch.nn.Module): Custom position encoding layer.
+    # Args:
+        # idim (int): Input dimension.
+        # odim (int): Output dimension.
+        # dropout_rate (float): Dropout rate.
+        # pos_enc (torch.nn.Module): Custom position encoding layer.
 
-    """
+    # """
 
-    def __init__(self, idim, odim, dropout_rate, pos_enc=None):
-        """Construct an Conv2dSubsampling2 object."""
-        super(Conv2dSubsampling2, self).__init__()
-        self.conv = torch.nn.Sequential(
-            torch.nn.Conv2d(1, odim, 3, 2),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(odim, odim, 3, 1),
-            torch.nn.ReLU(),
-        )
-        self.out = torch.nn.Sequential(
-            torch.nn.Linear(odim * (((idim - 1) // 2 - 2)), odim),
-            pos_enc if pos_enc is not None else PositionalEncoding(
-                odim, dropout_rate),
-        )
+    # def __init__(self, idim, odim, dropout_rate, pos_enc=None):
+        # """Construct an Conv2dSubsampling2 object."""
+        # super(Conv2dSubsampling2, self).__init__()
+        # self.conv = torch.nn.Sequential(
+            # torch.nn.Conv2d(1, odim, 3, 2),
+            # torch.nn.ReLU(),
+            # torch.nn.Conv2d(odim, odim, 3, 1),
+            # torch.nn.ReLU(),
+        # )
+        # self.out = torch.nn.Sequential(
+            # torch.nn.Linear(odim * (((idim - 1) // 2 - 2)), odim),
+            # pos_enc if pos_enc is not None else PositionalEncoding(
+                # odim, dropout_rate),
+        # )
 
-    def forward(self, x, x_mask):
-        """Subsample x.
+    # def forward(self, x, x_mask):
+        # """Subsample x.
 
-        Args:
-            x (torch.Tensor): Input tensor (#batch, time, idim).
-            x_mask (torch.Tensor): Input mask (#batch, 1, time).
+        # Args:
+            # x (torch.Tensor): Input tensor (#batch, time, idim).
+            # x_mask (torch.Tensor): Input mask (#batch, 1, time).
 
-        Returns:
-            torch.Tensor: Subsampled tensor (#batch, time', odim),
-                where time' = time // 2.
-            torch.Tensor: Subsampled mask (#batch, 1, time'),
-                where time' = time // 2.
+        # Returns:
+            # torch.Tensor: Subsampled tensor (#batch, time', odim),
+                # where time' = time // 2.
+            # torch.Tensor: Subsampled mask (#batch, 1, time'),
+                # where time' = time // 2.
 
-        """
-        x = x.unsqueeze(1)  # (b, c, t, f)
-        x = self.conv(x)
-        b, c, t, f = x.size()
-        x = self.out(x.transpose(1, 2).contiguous().view(b, t, c * f))
-        if x_mask is None:
-            return x, None
-        return x, x_mask[:, :, :-6:2]
+        # """
+        # x = x.unsqueeze(1)  # (b, c, t, f)
+        # x = self.conv(x)
+        # b, c, t, f = x.size()
+        # x = self.out(x.transpose(1, 2).contiguous().view(b, t, c * f))
+        # if x_mask is None:
+            # return x, None
+        # return x, x_mask[:, :, :-6:2]
 
-    def __getitem__(self, key):
-        """Get item.
+    # def __getitem__(self, key):
+        # """Get item.
 
-        When reset_parameters() is called, if use_scaled_pos_enc is used,
-            return the positioning encoding.
+        # When reset_parameters() is called, if use_scaled_pos_enc is used,
+            # return the positioning encoding.
 
-        """
-        if key != -1:
-            raise NotImplementedError(
-                "Support only `-1` (for `reset_parameters`).")
-        return self.out[key]
+        # """
+        # if key != -1:
+            # raise NotImplementedError(
+                # "Support only `-1` (for `reset_parameters`).")
+        # return self.out[key]
 
 
 class Conv2dSubsampling3(nn.Module):
