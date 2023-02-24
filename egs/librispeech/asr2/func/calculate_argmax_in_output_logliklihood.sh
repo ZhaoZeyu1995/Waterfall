ca2fb4b3c14296a46232104c1a9faf54a761ea3e
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2023-02-24 01:26:19 +0000

    add some comments in func/calculate_argmax_in_output_loglikelihood.sh

--- egs/librispeech/asr2/func/calculate_argmax_in_output_logliklihood.sh
+++ egs/librispeech/asr2/func/calculate_argmax_in_output_logliklihood.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 . ./path.sh
+. ./env.sh
 . ./cmd.sh
 
 loglikeli_scp=$1
 token_list=$2
+
+# usage : calculate_argmax_in_output_logliklihood.sh loglikeli_scp token_list report_output
 
 if [ -z $3 ]; then
     report_output="$(dirname $loglikeli_scp)/statistics_token.txt"
