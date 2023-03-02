6790762e7de625109d25eb8d14fd6ced3b8c3024
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2023-03-02 23:45:18 +0000

    add a new topo mmictc-1 that has a self-loop on the first state of mmictc.

---
+++ utils/get_token_fst_mmictc-1_k2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Apache 2.0
+
+
+'''
+(0) -> a0:a -> (a1:<eps>) -> (0)    Note: () represents self-loop, (0) represents the self-loop on state 0
+          -> - - - - - - > (0)
+'''
+
+import sys
+
+fread = open(sys.argv[1], 'r')
+
+
+nodeX = 1
+nodes= [] # list of tuple (phone, nodeX). Note each phone corresponds to one state id, nodeX
+
+
+print(str(0) + ' ' + str(0) + ' ' + '<blk>' + ' ' + '<eps>') # <blk> self-loop on state 0
+
+for entry in fread.readlines():
+    entry = entry.replace('\n', '').strip()
+    fields = entry.split(' ')
+    phone = fields[0]
+    if phone == '<eps>' or phone == '<SIL>' or phone.startswith('#'):
+        continue
+    else:
+        print(str(0) + ' ' + str(0) + ' ' + phone+'_0' + ' ' + phone) # transiting the first state
+        print(str(0) + ' ' + str(nodeX) + ' ' + phone+'_0' + ' ' + phone) # transiting the first state
+        print(str(nodeX) + ' ' + str(nodeX) + ' ' + phone+'_0' + ' ' + '<eps>') # first state self-loop
+        print(str(nodeX) + ' ' + str(0) + ' ' + phone+'_1' + ' ' + '<eps>') # transiting the first state
+        print(str(nodeX) + ' ' + str(nodeX+1) + ' ' + phone+'_1' + ' <eps>') #  transiting to the second state
+        print(str(nodeX+1) + ' ' + str(nodeX+1) + ' ' + phone+'_1' + ' <eps>') #  transiting to the second state
+        print(str(nodeX+1) + ' ' + str(0) + ' ' + phone+'_1' + ' <eps>') #  transiting to the second state
+    nodeX += 2
+
+print('0')
+
+
+fread.close()
