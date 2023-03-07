a1987e0e732916070a3c2d156d2932ac6fea8e6c
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2023-03-07 17:30:24 +0000

    add pymmictc decoder related files.

---
+++ egs/librispeech/asr2/func/decode_pymmictc.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+from kaldiio import ReadHelper
+import argparse
+import logging
+import torch
+import numpy as np
+import scipy
+from waterfall.decoder import pyctcdecode
+
+def process_token(token):
+    if token == '<SIL>':
+        return None
+    elif token == '<blk>':
+        return ''
+    elif token.endswith('_0'):
+        return token[:-2]
+    elif token.endswith('_1'):
+        return "%%" + token[:-2]
+
+def main(args):
+    words = None
+    if args.word_symbol_table:
+        words = []
+        with open(args.word_symbol_table) as f:
+            for line in f:
+                word = line.strip().split()[0]
+                if word != "<eps>":
+                    words.append(word)
+
+    tokens = []
+    with open(args.tokens) as f:
+        for line in f:
+            token = line.strip().split()[0]
+            token = process_token(token)
+            if token is not None:
+                tokens.append(token)
+
+    loglikeli_scp = args.log_likelihood
+
+    decoder = pyctcdecode.decoder.build_mmictcblkdecoder(
+        labels=tokens, kenlm_model_path=args.lm, unigrams=words, alpha=args.alpha, beta=args.beta)
+
+
+    output_f = open(args.word_output, 'w')
+    output_f_c = ''
+    with ReadHelper('scp:%s' % (loglikeli_scp)) as reader:
+        for utt, log_likelihood in reader:
+            text = decoder.decode(log_likelihood,
+                                  beam_width=args.beam_width,
+                                  beam_prune_logp=args.beam_prune_logp,
+                                  token_min_logp=args.token_min_logp)
+            output_f_c += '%s %s\n' % (utt, text.strip())
+            logging.info('%s %s' % (utt, text.strip()))
+    output_f.write(output_f_c)
+    output_f.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='This programme conducts the prefix beam search algorithm with a given log_likelihood scp file.')
+
+    parser.add_argument(
+        'log_likelihood', help='The path of log_likelihood scp', type=str)
+    parser.add_argument(
+        'word_output', help='The path for saving output word sequences', type=str)
+    # parser.add_argument('--tokens', help='The token file in the correct format. <token>', type=str, default='/disk/scratch3/zzhao/espnet/egs/librispeech/asr4/data/lang_char/train_960_unigram5000_units.txt')
+    parser.add_argument('tokens', help='The token file in the correct format. <token>',
+                        type=str)
+    parser.add_argument('--lm', help='The language model directory',
+                        type=str)
+
+    parser.add_argument(
+        '--alpha', help='The language model weight', type=float, default=0.5)
+    parser.add_argument(
+        '--beta', help='The word sequence length weight', type=float, default=1.5)
+    parser.add_argument(
+        '--beam_width', help='The maximum number of beams at each step in decoding', type=int, default=100)
+    parser.add_argument(
+        '--beam_prune_logp', help='Beams that are much worse than best beam will be pruned.', type=float, default=-10.0)
+    parser.add_argument(
+        '--token_min_logp', help='Tokens below this logp are skipped unless they are argmax of frame', type=float, default=-5.0)
+
+    parser.add_argument('--word_symbol_table',
+                        help='The path of the word symbol table, by which the output label ids can be translated into words', type=str, default=None)
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    main(args)
---
+++ egs/librispeech/asr2/func/decode_pymmictc.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+
+. ./path.sh
+. ./cmd.sh
+
+# Decoding with FST-based decoder
+# e.g. decode_pymmictc.sh data/test_dev93 data/lang exp/mmictc_LV60K_2l/version_3/decode_test_dev93
+
+
+nj=10
+alpha="0.5"
+beta="1.5"
+beam_width='50'
+beam_prune_logp="-10"
+token_min_logp="-5"
+lm=
+
+. ./utils/parse_options.sh
+
+if [ $# != 3 ]; then
+  echo "Usage: decode_pymmictc.sh [options] <data_dir> <lang_dir> <predict_dir>"
+  echo "     --nj                        # default: 10, the number of jobs."
+  echo "e.g.:"
+  echo " $0 data/test_dev93 data/lang_tg exp/mmictc_LV60K_2l/version_3/decode_test_dev93"
+  echo " where <predict_dir> should contain a output.1.scp file storing all of the output posterior probabilities."
+  echo " unless there is splitN dir storing all split scp files."
+  exit 1
+fi
+
+data_dir=$1
+lang_dir=$2
+predict_dir=$3
+
+
+# split the output.1.scp
+if [ ! -d $predict_dir/split$nj ]; then
+    if [ -f $predict_dir/output.1.scp ]; then
+        run.pl JOB=1:$nj $predict_dir/split$nj/log/split_scp.JOB.log utils/split_scp.pl -j $nj JOB --one-based $predict_dir/output.1.scp $predict_dir/split$nj/output.JOB.scp
+    else
+        echo "Cannot find output.1.scp or split$nj in $predict_dir!!! There should be one of them at least!"
+    fi
+fi
+
+decode_dir=$predict_dir/alpha_${alpha}-beta_${beta}-beam_width_${beam_width}-beam_prune_${beam_prune_logp}-token_min_logp_${token_min_logp}
+mkdir -p $decode_dir
+if [ -z $lm ]; then
+    run.pl JOB=1:$nj $decode_dir/split${nj}/log/decode_pymmictc.JOB.log func/decode_pymmictc.py --alpha $alpha --beta $beta --beam_width $beam_width --beam_prune_logp ${beam_prune_logp} --token_min_logp ${token_min_logp} --word_symbol_table ${lang_dir}/words.txt $predict_dir/split$nj/output.JOB.scp  $decode_dir/split$nj/hyp.JOB.wrd $lang_dir/k2/tokens.txt 
+else
+    run.pl JOB=1:$nj $decode_dir/split${nj}/log/decode_pymmictc.JOB.log func/decode_pymmictc.py --alpha $alpha --beta $beta --beam_width $beam_width --beam_prune_logp ${beam_prune_logp} --token_min_logp ${token_min_logp} --lm $lm --word_symbol_table ${lang_dir}/words.txt $predict_dir/split$nj/output.JOB.scp  $decode_dir/split$nj/hyp.JOB.wrd $lang_dir/k2/tokens.txt 
+fi
+
+for i in $(seq $nj); do
+    cat $decode_dir/split$nj/hyp.$i.wrd || exit 1;
+done > $decode_dir/hyp.wrd.txt || exit 1
+
+post_process_decode.py $decode_dir/hyp.wrd.txt $data_dir/utt2spk > $decode_dir/hyp.wrd.trn
+
+sclite -r $predict_dir/ref.wrd.trn.1 trn -h $decode_dir/hyp.wrd.trn trn -i rm -o all stdout > $decode_dir/results.wrd.txt
+
